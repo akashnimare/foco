3600e4cc362fdbd6262253abf10b28fc4b851127
Akash Nimare <svnitakash@gmail.com>
2018-04-16 12:37:47 +0530

    ES6 + code refactor.

--- app/js/app.js
+++ app/js/app.js
@@ -3,64 +3,42 @@ const { remote } = require("electron");
 // Build Player section
 const domSounds = document.getElementsByTagName("ul")[0].children;
 
-Object.prototype.extend = function(extension) {
-  let object = Object.create(this);
-  for (let property in extension) {
-    if (
-      extension.hasOwnProperty(property) ||
-      object[property] === "undefined"
-    ) {
-      object[property] = extension[property];
-    }
+class Create {
+  constructor(switchEl, volumeEl, audioEl) {
+    this.switchEl = switchEl;
+    this.volumeEl = volumeEl;
+    this.audioEl = audioEl;
+    this.isPlaying = false;
+    this.switchEl.addEventListener("click", this.toggle.bind(this));
+    this.volumeEl.onchange = this.adjustVolume.bind(this);
   }
-  return object;
-};
-
-let Sound = {
-  create(switchEl, volumeEl, audioEl) {
-    const newObj = this.extend({
-      switchEl: switchEl,
-      volumeEl: volumeEl,
-      audioEl: audioEl,
-      isPlaying: false
-    });
-
-    newObj.switchEl.addEventListener("click", newObj.toggle.bind(newObj));
-    newObj.volumeEl.onchange = newObj.adjustVolume.bind(newObj);
-
-    return newObj;
-  },
   toggle() {
     console.log("toggle fired");
-    if (this.isPlaying) {
-      this.pause();
-    } else {
-      this.play();
-    }
+    this.isPlaying ? this.pause() : this.play();
     this.isPlaying = !this.isPlaying;
-  },
+  }
   pause() {
     this.audioEl.stop;
     this.switchEl.checked = false;
     this.audioEl.pause();
     console.log("stop");
-  },
+  }
   play() {
     console.log("start");
     this.switchEl.checked = true;
     this.audioEl.play("hello", this.switchEl.checked);
-  },
+  }
   adjustVolume(e) {
     console.log("volume adjusted");
     this.audioEl.volume = this.volumeEl.value / 100;
   }
-};
+}
 
 const sounds = [];
 
 Array.from(domSounds).forEach(domSound => {
   sounds.push(
-    Sound.create(
+    new Create(
       domSound.children[3].children[0],
       domSound.children[3].children[2],
       domSound.children[2]
